75e624ff3e5fc360e3598f3639634f6a55557f2c
Ryan York <ryanyork@Ryans-MacBook-Pro.local>
2024-07-30 11:40:44 -0700

    update envs

--- envs/install_r_packages.R
+++ envs/install_r_packages.R
@@ -1,3 +1,9 @@
 library(devtools)
 
 devtools::install_github("Arcadia-Science/arcadia-color-brewer")
+install.packages("taxonomizr", version = "0.10.2")
+install.packages("packcircles", version = "0.3.6")
+install.packages("rsqlite", version = "2.3.0")
+install.packages("sm", version = "2.2")
+install.packages("taxizedb", version = "0.3.1")
+install.packages("vioplot", version = "0.5.0")
